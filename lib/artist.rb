7f4729d74badf9c03bcabf94c5b9d6d7356f93a4
shmuly11 <shmuly11@gmail.com>
2020-12-15 10:26:55 -0500

    commit tree e8fc0f9421f1d19d1c43b4ef4d891a56c2b57b92

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,24 @@
+class Artist
+    @@all =[]
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self}
+    end
+
+    def genres
+        self.songs.map(&:genre)
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,21 @@
+class Genre
+    @@all = []
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| song.genre == self}
+    end
+
+    def artists
+        self.songs.map(&:artist)
+    end
+
+end
